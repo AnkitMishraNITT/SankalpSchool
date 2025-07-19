c35bcfb363dc3da8f62422ccdb7b8cc99e633850
AnkitMishraNITT <Ankitmishra1737@gmail.com>
2025-07-19 20:05:50 +0530

    removed errors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: 'https://ankitmishranitt.github.io/SankalpSchool/', // 👈 IMPORTANT: use your repo name here
 })
+
+
